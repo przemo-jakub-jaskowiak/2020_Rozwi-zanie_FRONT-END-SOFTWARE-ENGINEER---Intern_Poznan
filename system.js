dc243f641d9981e8d8d69adac5d8398a777059ee
Przemek <p.j.jaskowiak@gmail.com>
2020-04-16 23:28:47 +0200

    version1

---
+++ system.js
@@ -0,0 +1,74 @@
+const pokedex = document.getElementById('pokedex');
+const pokeCache = {};
+
+const fetchPokemon = async () => {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((result, index) => ({
+        ...result,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+
+    }));
+
+    displayPokemon(pokemon);
+
+};
+const displayPokemon = (pokemon) => {
+
+    const pokemonHTMLString = pokemon.map(pokeman => `<li class="card" onclick="selectPokemon(${pokeman.id})">
+    <img class="card-image" src="${pokeman.image}"/>
+    <h2 class="card-title">${pokeman.name}</h2>
+    
+    </li>`).join('');
+    pokedex.innerHTML = pokemonHTMLString;
+}
+
+const selectPokemon = async (id) => {
+    if (!pokeCache[id]) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url);
+        const pokeman = await res.json();
+        pokeCache[id] = pokeman;
+
+        displayPopup(pokeman);
+    }
+    displayPopup(pokeCache[id]);
+
+};
+
+const displayPopup = (pokeman) => {
+    const type = pokeman.types.map(type => type.type.name).join(', ');
+    const image = pokeman.sprites['front_default'];
+    const htmlString = `<div class="popup"><button id="closeBtn" onclick="closePopup()">Close</button>
+        <div class="card">
+            <img class="card-image" src="${image}"/>
+            <h2 class="card-title">${pokeman.id}.${pokeman.name}</h2>
+            <p><small>Height: </small>${pokeman.height} | <small>Weight: </small>${pokeman.weight} | <small>Type: </small>${type} </p>
+        </div>
+    </div>`;
+    pokedex.innerHTML = htmlString + pokedex.innerHTML;
+};
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+};
+
+fetchPokemon();
+
+
+var ball = document.querySelector(".poke");
+
+function loke() {
+    this.classList.toggle('move')
+
+}
+ball.addEventListener('click', loke)
+
+var lot = document.querySelector('.arr');
+
+lot.addEventListener('click', function () {
+    window.scrollTo(0, 0);
+})
